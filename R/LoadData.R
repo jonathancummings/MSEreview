ade5aefd05c5ae8be528e5d79a4d6338e83310ad
Jonathan Cummings <jonathan.cummings@gmail.com>
2021-02-16 17:53:55 -0500

    Create separate script to load data from excel files and save them to an .RData file

---
+++ R/LoadData.R
@@ -0,0 +1,22 @@
+# MSE Problem Framing 
+# Load MSE review data from Excel files into R
+# 2/16/2021, JWC
+
+##### 
+# Meta Data and setup
+# Description: Import excel file data to R to create a .RData file for use in other scripts
+
+# load libraries
+library(tidyverse) # upgrade to base R
+library(readxl) # read in excel files
+
+# Obtain data tables
+study<-read_xlsx("data/DB files - Excel/tblStudy.xlsx")
+mgmt<-read_xlsx("data/DB files - Excel/tblStudyManagementTools.xlsx")
+mgmt$fkStudyID<-parse_integer(mgmt$fkStudyID)
+obj<-read_xlsx("data/DB files - Excel/tblStudyObjectives.xlsx")
+obj$fkStudyID<-parse_integer(obj$fkStudyID)
+fields<-read_xlsx("data/DB files - Excel/tblStudyFields.xlsx")
+
+# save .RData file
+save.image(file = "data/MSEreview.RData")
